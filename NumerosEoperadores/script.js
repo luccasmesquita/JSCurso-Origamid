03f7253cc11172eca7db16416aab972547ad4876
Luccas Mesquita <luccas.m.mesquita@hotmail.com>
2021-03-07 19:07:18 +0100

    c05 - aula numeros e operadores 1

---
+++ NumerosEoperadores/script.js
@@ -0,0 +1,15 @@
+// Numeros 
+
+var idade = 27;
+var gols = 1000;
+var pi = 3.14; // não se usa virgula e sim ponto
+var expo = 2e10; // colocar quantidades de zero apos o "e" 
+
+//Operadores aritiméticos
+
+var soma = 100 + 100; //200
+var subtracao = 100 - 50; //50
+var multiplicacao = 2 * 2; //4
+var divisao = 10 / 2 ; //5
+var expoente = 2 ** 4 // 16
+var modulo = 14 % 5; //4

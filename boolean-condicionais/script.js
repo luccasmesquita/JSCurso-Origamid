2fa12ce0efcd84954cec032cc01c03e3c546dc9b
Luccas Mesquita <luccas.m.mesquita@hotmail.com>
2021-03-15 16:26:18 +0100

    c09 - continuação aula condicionais

--- boolean-condicionais/script.js
+++ boolean-condicionais/script.js
@@ -25,3 +25,45 @@ apenas um = é atribuição
 O == faz comparação não tão estrita o === faz uma comparação estrita, ou seja 
 os tipos de dados devem ser iguais quando usamos o ===
 */
+
+/* Operadores Lógicos &&
+&& são operadores de comparação, comprara se uma expressão e outra são verdadeiras
+ele sempre vai retornar a expressão falsa, ou a ultima verdadeira
+*/
+
+true && true; //true
+true && false; // false
+false && true; // false
+"Gato" && "Cão"; // "Cão" pois foi a ultima verdadeira 
+
+/* Operadores Lógicos ||
+|| são operadores de comparação, comprara se uma expressão ou outra é verdadeiras
+se alguma das expressões forem verdadeiras, ele sempre vai retornar o verdadeiro
+*/
+
+true || true; // true
+true || false; // true
+false || true; // true
+"Gato" || "Cão"; // "Gato" pois é a primeira verdadeira
+
+/* Switch
+com switch voce pode verificar se uma variavel é igual a diferentes valores 
+utilizando o case. Caso ela seja igual, voce pode fazer alguma coisa e usar a palavra chave break;
+para cancelar a continuação. O valor de default ocorrerá caso nenhuma das anteriores seja verdadeira.
+*/
+
+var CorFavorita = "Azul";
+
+switch (CorFavorita) {
+    case 'Azul':
+        console.log("Olhe para o céu");
+        break;
+    case 'Verelho':
+        console.log('Olhe para as rosas');
+        break;
+    case 'Amarelo':
+        console.log("Olhe para o sol")
+        break;
+    default:
+        console.log('Feche os olhos');    
+}

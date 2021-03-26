ef8855615971de15462616c603228be1fa15ce27
Luccas Mesquita <luccas.m.mesquita@hotmail.com>
2021-03-26 17:27:02 +0100

    curso rocketseat - aula sobre object

---
+++ Rocketsaet/TiposDados/Object/script.js
@@ -0,0 +1,15 @@
+/* 
+Object
+
+    é um objeto
+ que tem propriedades / atributos
+ e funcionalidades / métodos
+
+ {propriedade: "valor"}
+*/
+ 
+console.log({
+    nome: "Luccas",
+    idade: 27
+}); // criei um objeto que tem uma propriedade chamada nome o com um valor Luccas
+ // e uma propriedade chamada idade o com um valor 27

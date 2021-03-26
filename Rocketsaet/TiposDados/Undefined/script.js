83ea947ac533c368224621773a7fc6de0541e96a
Luccas Mesquita <luccas.m.mesquita@hotmail.com>
2021-03-26 15:18:51 +0100

    curso rocketseat - aula sobre undefined vs null

---
+++ Rocketsaet/TiposDados/Undefined/script.js
@@ -0,0 +1,13 @@
+/* 
+Undefined
+
+    é um valor indefinido
+
+Null
+    Nullo
+    objeto que não possui nada dentro
+    diferente de indefinido
+*/
+ 
+console.log(undefined) // é um valor que não vai existir
+console.log(null) // é um valor um valor vazio

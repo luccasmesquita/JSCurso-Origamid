22f22e48d9f7a6d08e5f2cc88d2e427a95b39b34
Luccas Mesquita <luccas.m.mesquita@hotmail.com>
2021-03-26 15:05:12 +0100

    curso rocketseat - aula sobre number

---
+++ Rocketsaet/TiposDados/Number/script.js
@@ -0,0 +1,18 @@
+/* 
+Number
+
+number são os números
+e tem varios tipos de numeros
+
+33 / numero inteiro
+12.5 / reais
+NaN / Not a Number
+Infinity / infinito (tem que ser com I maiusculo)
+
+*/
+ 
+console.log(33) // o tipo number não precisa ser colocado entre aspas
+
+console.log(12 / "abc"); // aqui vai dar NaN pois ele não consegue dividir um numero com uma string
+
+

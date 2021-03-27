87d30eb64dba18462236023303b4623616c5316b
Luccas Mesquita <luccas.m.mesquita@hotmail.com>
2021-03-27 14:52:56 +0100

    atualização de caminhos dos arquivos html -> js

---
+++ Rocketsaet/Variaveis/script.js
@@ -0,0 +1,19 @@
+/* 
+Variaveis
+
+    Nomes simbolicos pare receber um valor
+    Atalho para codigos
+    Identificadores
+
+    3 Palavras reservadas para criar uma variavel
+        var
+        let
+        conts
+ */
+ 
+var clima = "Quente"; // a variavel declarada recebe o valor quente (o igual é atribuição e não comparação)
+clima = "Frio"; // com var e let eu posso alterar o valor durante a aplicação
+
+console.log(clima);
+
+const idade = 27; // a variavel criada com const vai ser um valor constante então, não pode ser alterado

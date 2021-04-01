5900f50f54f718afe6ac1caf15aad0d6ead3889c
Luccas Mesquita <luccas.m.mesquita@hotmail.com>
2021-04-01 15:59:54 +0200

    curso coder - aula sobre variaveis

---
+++ Coder/Fundamentos/variaveis.js
@@ -0,0 +1,12 @@
+//Formas de criar variaveis
+
+var num = 3; //aqui estou dizendo que a variavel com nome "num" recebe o valor "3" é uma atribuição para
+// ocupar um endereço na memoria
+//com var podemor redeclarar a variavel 
+//ex:
+var num = 5; // estou redefinindo um valor para a variavel
+
+let num2 = 4; // criação de variaveis com let
+
+const num3 = 300; // criando variavel com const, como o nome ja diz, essa tipo de variavel é uma constante
+// não pode ser alterada, não podemos atribuir outro valor

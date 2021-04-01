36b32c6b091f59ac05757a2a300a5821f34084b3
Luccas Mesquita <luccas.m.mesquita@hotmail.com>
2021-04-01 16:11:06 +0200

    curso coder - aula sobre tipagem

---
+++ Coder/Fundamentos/tipagem.js
@@ -0,0 +1,12 @@
+// Jaascript é uma liguagem com tipagem fraca, ou seja eu consigo alterar o tipo da variavel
+// posso alterar uma variavel string para uma variavel number ...
+
+let qualquer = "legal"; //definindo variavel do tipo string ( é uma variavel do tipo de texto)
+console.log(qualquer); // aqui estou mostrando no console a variavel
+console.log(typeof qualquer); //aqui estou mostrando no console o tipo da variavel (string / number/ boolean)
+
+qualquer = 3.1547; // alterando o tipo da variavel de string para number
+console.log(qualquer); // aqui estou mostrando no console a variavel
+console.log(typeof qualquer); //aqui estou mostrando no console o tipo da variavel (string / number/ boolean)
+
+//evite escolher nomes genericos e siglas para variaveis

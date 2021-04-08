b2359e15b724290457c3ac1febc3068aa5dc6d54
Luccas Mesquita <luccas.m.mesquita@hotmail.com>
2021-04-08 21:00:39 +0200

    curso coder - aula sobre boolean

---
+++ Coder/Fundamentos/boolean.js
@@ -0,0 +1,31 @@
+// tipos de dados, um deles é o boolean que me retorna se é true ou false
+// o zero (0) é sempre falso / false
+let isAtivo = false; // isso é uma variavel booleana com o valor falso
+console.log(isAtivo);
+
+isAtivo = true; // isso é uma variavel booleana com o valor true
+console.log(isAtivo)
+
+isAtivo = 1; //podemos usar um valor numerico para representar um dado booleano
+console.log(!!isAtivo); // colocando o "!" é equivalente é conferir se é negativo, colocando duas vezes verifica o valor correto
+
+console.log('Os verdadeiros'); //todos os numeros inteiros é verdadeiro
+console.log(!!3);
+console.log(!!-1);
+console.log(!!' '); //um espaço vazio é verdadeiro
+console.log(!![]); //um array é verdadeiro 
+console.log(!!{}); //um objeto é verdadeiro 
+
+console.log('Os Falsos'); //todos os numeros inteiros é verdadeiro
+console.log(!!0);
+console.log(!!''); //uma string vazia é falso
+console.log(!!null);
+console.log(!!NaN);
+console.log(!!undefined);
+
+
+
+
+
+
+

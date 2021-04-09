cc667fd835a03cae7c90efe6cab9c5ddf3189cba
Luccas Mesquita <luccas.m.mesquita@hotmail.com>
2021-04-09 22:16:29 +0200

    curso coder - aula sobre objetos

---
+++ Coder/Fundamentos/objetos.js
@@ -0,0 +1,18 @@
+/**
+ * Objetos
+ * objetos em javascript é uma coleção de chave e valor
+ * você tem um nome do atributo/identificador  e você passa o valor pra ele
+ */
+
+const prod1 = {} // definir um objeto vazio
+prod1.nome = 'Celular'; //aqui definimos que dentro desse objeto vai ter uma chave/identificador chamado nome
+// e um valor 'Celular'
+prod1.preco = 4999.90;
+
+console.log(prod1);
+
+const prod2 = {
+    nome = 'Camisa Polo',
+    valor = 79.90
+} //essa é a forma mais utilizada para definir objetos 
+console.log(prod2);
